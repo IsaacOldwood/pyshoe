866f2776cd5206ad8bd91e2fc3a479d53bb66699
Isaac Oldwood <Isaac.Oldwood@gmail.com>
2026-02-01 15:49:19 +0000

    chore: Add comments

--- iteration_1/main.py
+++ iteration_1/main.py
@@ -1,20 +1,26 @@
 import pyautogui
 from time import sleep
 
+# Open Chrome
 pyautogui.press("win")
 
 pyautogui.write("Chrome", interval=0.1)
 pyautogui.press("enter")
 
+# Wait for Chrome to open
 sleep(2)
 
+# Navigate to website
 pyautogui.write("yeezy.com", interval=0.1)
 pyautogui.press("enter")
 
+# Wait for page to load
 sleep(5)
 
+# Click the product
 pyautogui.click(300, 300)
 
+# Click add to cart button
 pyautogui.click(1500, 800)
 
 # ...
--- iteration_2/main.py
+++ iteration_2/main.py
@@ -3,22 +3,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+# Open webpage and setup wait
 driver = webdriver.Chrome()
 driver.get("https://yeezy.com/")
-
 wait = WebDriverWait(driver, 5)
 
+# Find the product button
 driver.find_element(By.CSS_SELECTOR, 'button[aria-label="View PK-01"]').click()
 
+# Wait for the add to cart button and click
 line = wait.until(EC.presence_of_element_located((By.TAG_NAME, "line")))
 line.find_element(By.XPATH, "..").click()
 
+# Wait for the size choice and click
 wait.until(EC.presence_of_element_located((By.CLASS_NAME, "garment-size-label")))
 size = driver.find_elements(By.CLASS_NAME, "garment-size-label")[2]
 size.find_element(By.XPATH, "../..").click()
 
+# Open the cart
 driver.find_element(By.ID, "cart-button").click()
 
+# Fill in payment details
 email_input = wait.until(EC.element_to_be_clickable((By.ID, "contact-email")))
 email_input.click()
 email_input.send_keys("me@email.com")
+
+# ...
